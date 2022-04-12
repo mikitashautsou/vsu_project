fac4b6a6512297193a7069d60504f26869421cad
Andrei Tsybulski <andrei.tsybulski@innowise-group.com>
2022-04-12 23:35:10 +0300

    Init asp.net core project

---
+++ asp/Models/Fine.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fines.Models
+{
+	public class Fine
+	{
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string UserId { get; set; }
+		[Display(Name = "Description")]
+		public string Description { get; set; }
+		[Display(Name = "Fine Amount")]
+		public int FineAmount { get; set; }
+		[Display(Name = "Status")]
+		public Status Status { get; set; }
+	}
+
+	public enum Status
+	{
+		Paid,
+		NotPaid
+	}
+}
---
+++ asp/Services/FineService.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System.Threading.Tasks;
+using Fines.Models;
+
+namespace Fines.Services
+{
+    public class FineService
+    {
+        IGridFSBucket gridFS;   
+        IMongoCollection<Fine> fines; 
+
+        public FineService()
+        {
+            string connectionString = "mongodb://127.0.0.1:27017";
+            var connection = new MongoUrlBuilder(connectionString);
+
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase database = client.GetDatabase(connection.DatabaseName);
+
+            gridFS = new GridFSBucket(database);
+            fines = database.GetCollection<Fine>("Fines");
+        }
+
+        public async Task<Fine> GetFine(string id)
+        {
+            return await fines.Find(new BsonDocument("_id", new ObjectId(id))).FirstOrDefaultAsync();
+        }
+
+        public async Task Create(Fine fine)
+        {
+            await fines.InsertOneAsync(fine);
+        }
+
+        public async Task Update(Fine fine)
+        {
+            await fines.ReplaceOneAsync(new BsonDocument("_id", new ObjectId(fine.UserId)), fine);
+        }
+
+        public async Task Remove(string id)
+        {
+            await fines.DeleteOneAsync(new BsonDocument("_id", new ObjectId(id)));
+        }
+    }
+}
